4a4fad609a52409332deb426bf9f3b32df889066
Pete Goodman <thegingerbloke@users.noreply.github.com>
2017-03-29 12:26:59 +0100

    Add text ticker to Hero component

--- js/site.js
+++ js/site.js
@@ -5,6 +5,11 @@
  */
 (function () {
 
+  function init () {
+    addWelcomeConsoleLog();
+    animateHeroTicker();
+  }
+
   function addWelcomeConsoleLog () {
     if (console && console.log) {
       var logs = [
@@ -26,8 +31,62 @@
     }
   }
 
-  function init () {
-    addWelcomeConsoleLog();
+  // adapted from: https://codepen.io/testshoot/pen/BjWPZq
+  function animateHeroTicker () {
+    var words = document.querySelectorAll('.js-Hero-tickerWord');
+    var wordArray = [];
+    var currentWordIndex = 0;
+
+    words[currentWordIndex].style.opacity = 1;
+    for (var i = 0; i < words.length; i++) {
+      splitLetters(words[i]);
+    }
+
+    function changeWord () {
+      var currentWord = wordArray[currentWordIndex];
+      var nextWord = currentWordIndex === words.length - 1 ? wordArray[0] : wordArray[currentWordIndex + 1];
+      for (var i = 0; i < currentWord.length; i++) {
+        animateLetterOut(currentWord, i);
+      }
+
+      for (var i = 0; i < nextWord.length; i++) {
+        nextWord[i].className = 'Hero-tickerLetter behind';
+        nextWord[0].parentElement.style.opacity = 1;
+        animateLetterIn(nextWord, i);
+      }
+
+      currentWordIndex = (currentWordIndex === wordArray.length - 1) ? 0 : currentWordIndex + 1;
+      setTimeout(changeWord, 4000);
+    }
+
+    function animateLetterOut (currentWord, i) {
+      setTimeout(function() {
+    		currentWord[i].className = 'Hero-tickerLetter out';
+      }, i * 80);
+    }
+
+    function animateLetterIn (nextWord, i) {
+      setTimeout(function() {
+    		nextWord[i].className = 'Hero-tickerLetter in';
+      }, 340 + (i * 80));
+    }
+
+    function splitLetters (word) {
+      var content = word.innerHTML;
+      word.innerHTML = '';
+      var letters = [];
+      for (var i = 0; i < content.length; i++) {
+        var letter = document.createElement('span');
+        letter.className = 'Hero-tickerLetter';
+        letter.innerHTML = content.charAt(i);
+        word.appendChild(letter);
+        letters.push(letter);
+      }
+
+      wordArray.push(letters);
+    }
+
+    changeWord();
   }
 
   init();
